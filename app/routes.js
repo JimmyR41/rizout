f98192f1582814f36262d35b88cbcd283e8f42a2
Jimmy Riccelli <jrriccelli@gmail.com>
2017-01-19 18:24:36 -0500

    Cleaned up routes

--- app/routes.js
+++ app/routes.js
@@ -6,28 +6,38 @@ var path = require('path');
 var router = express.Router();
 module.exports = router;
 
-//assign routes
-router.get('/', function(req,res){
+//Route protoypes
+router.get('/', homePage);
+router.get('/about', aboutPage);
+router.get('/contact', contactPage);
+router.post('/contact', contactPost);
+router.get('/@:username', homeParams);
+/*show 404*/
+router.use(show404);
+
+//route functions
+function homePage(req,res){
   res.render('pages/index');
-});
+};
 
-router.get('/about', function(req,res){
+function aboutPage(req,res){
   res.render('pages/about');
-});
+};
 
-router.get('/contact', function(req,res){
+function contactPage(req,res){
   res.render('pages/contact');
-});
+};
 
-router.post('/contact', function(req,res){
+function contactPost(req,res){
   console.log(req.body);
-  res.send("<h1>Hello, " + req.body.name + " Welcome to the club!</h1>")
-});
+  res.send("<h1>Hello, " + req.body.name + "Welcome to the club!</h1>")
+};
 
-router.get('/@:username', function(req,res){
+function homeParams(req,res){
   res.send('Welcome ' + req.params.username);
-});
+};
 
-router.use(function(req,res){
-  res.render('/pages/404');
-})
+function show404(req,res,next){
+  res.status(404);
+  res.render('pages/404');
+};
